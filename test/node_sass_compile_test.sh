533cd3a5078daf74646df83b28b87467eb0b4d32
kayo5994 <330956999@qq.com>
2018-11-08 17:46:29 +0800

    update: fixed test script of node-sass

--- test/node_sass_compile_test.sh
+++ test/node_sass_compile_test.sh
@@ -3,5 +3,5 @@
 # Test compilation with node-sass binary
 
 mkdir -p tmp/node-sass
-node-sass test/dummy_node/test.scss -o tmp/node-sass/test.css && \
+node-sass test/dummy_node/test.scss -o tmp/node-sass || \
 (echo "node-sass compilation failed" && exit 1)
